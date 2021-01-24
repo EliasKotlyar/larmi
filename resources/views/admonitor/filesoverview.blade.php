67502f97f7ca706a03e8e435ab869ec618a90aae
Elias Kotlyar <elias.kotlyar@alpha.pet>
2021-01-24 15:36:45 +0100

    Design

--- resources/views/admonitor/filesoverview.blade.php
+++ resources/views/admonitor/filesoverview.blade.php
@@ -18,8 +18,3 @@
         </div>
     </div>
 @endsection
-
-@push('js')
-    <script src="{{ asset('argon') }}/vendor/chart.js/dist/Chart.min.js"></script>
-    <script src="{{ asset('argon') }}/vendor/chart.js/dist/Chart.extension.js"></script>
-@endpush
